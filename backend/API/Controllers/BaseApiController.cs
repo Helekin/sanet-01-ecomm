c135dceee4f86294b9ab1204d15092a0b7a8ae13
helekin <clsjos2294@gmail.com>
2025-03-04 11:05:00 -0500

    FEAT: pagination, sort & search

---
+++ backend/API/Controllers/BaseApiController.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+using API.RequestHelpers;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    protected async Task<ActionResult> CreatePageResult<T>(IGenericRepository<T> repo, ISpecification<T> spec, int pageIndex, int pageSize) where T : BaseEntity
+    {
+        var items = await repo.ListAsync(spec);
+
+        var count = await repo.CountAsync(spec);
+
+        var pagination = new Pagination<T>(pageIndex, pageSize, count, items);
+
+        return Ok(pagination);
+    }
+}
